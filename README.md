fae8091a6a24feaf49c85cad43751aceaf2b3328
Edouard PAGNIER <edouard@nullpointerexcepto.com>
2018-08-07 21:55:29 +0200

    add setLocale and unit tests

--- README.md
+++ README.md
@@ -1,3 +1,90 @@
-# react-simple-i18n
+# react-light-i18n
 
-A light weight library to deal with string translation.
+A very light weight library to deal with string translation with automatic locale detection.
+
+## Usage
+
+```javascript
+import I18n from 'react-light-i18n'
+
+I18n.setTranslations({
+    en: {
+        'HELLO': 'Hello',
+        'GOODBYE': 'Goodbye'
+    },
+    fr: {
+        'HELLO': 'Bonjour',
+        'GOODBYE': 'Au revoir'
+    }
+})
+
+class Login extends Component <Props, State> {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{I18n.t('HELLO')}</p>
+                <p>{I18n.t('GOODBYE')}</p>
+            </div>
+        )
+    }
+}
+```
+
+You can also use seprated JSON file to store translations:
+
+```javascript
+import I18n from 'react-light-i18n'
+
+I18n.setTranslations({
+    en: require('./translations/en'),
+    fr: require('./translations/fr')
+})
+
+class Login extends Component <Props, State> {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{I18n.t('HELLO')}</p>
+                <p>{I18n.t('GOODBYE')}</p>
+            </div>
+        )
+    }
+}
+```
+
+####./translations/en.json
+
+```javascript
+{
+    "HELLO": "Hello",
+    "GOODBYE": "Goodbye"
+}
+```
+
+####./translations/fr.json
+
+```javascript
+{
+    "HELLO": "Bonjour",
+    "GOODBYE": "Au revoir"
+}
+```
+
+## Locale auto detection
+
+By defaul, **react-light-i18n** automatically detect the browser locale.
+Alternatively, you can force the locale to be use like this:
+
+```javascript
+I18n.setLocale('en')
+```
+
+In current version, locale like **en-US** or **fr-FR will** respectively fallback to **en** and **fr**.
--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
-import getLocale from 'browser-locale'
-
 export default class I18n {
 
     static translations
-    static locale
+	static locale
+	static languageFallback
 
-    static setTranslations(translations) {
-        this.translations = translations
-        let tmpLocale = getLocale()
-        this.locale = tmpLocale.includes("-") ? tmpLocale.split("-")[0] : tmpLocale
-    }
+    static setTranslations(translations, fallback = undefined) {
+		this.translations = translations
+		if (this.locale === undefined) {
+			let tmpLocale = getLocale()
+			this.locale = tmpLocale.includes("-") ? tmpLocale.split("-")[0] : tmpLocale
+		}
+		this.languageFallback = fallback === undefined ? 'en' : fallback
+	}
+	
+	static setLocale(locale) {
+		this.locale = locale.includes("-") ? locale.split("-")[0] : locale
+	}
 
     static t(key) {
-    	if(this.locale in this.translations) {
-    		if(key in this.translations[this.locale]) {
-    			return this.translations[this.locale][key]
-    		} else {
-    			return 'No ' + this.locale + ' translation for ' + key
-    		}
-    	} else {
-    		return 'No translation for ' + this.locale
-    	}
-    }
+		if (this.translations !== undefined) {
+			if(this.locale in this.translations) {
+				if(key in this.translations[this.locale]) {
+					return this.translations[this.locale][key]
+				} else {
+					return 'No ' + this.locale + ' translation for ' + key
+				}
+			} else {
+				if(key in this.translations[this.languageFallback]) {
+					return this.translations[this.languageFallback][key]
+				} else {
+					return 'No ' + this.locale + ' translation for ' + key
+				}
+			}
+		} else {
+			console.log('ERROR: No translation defined, consider calling setTranslations() first.')
+		}    	
+	}
+	
+	static getLocale() {
+		let locale
+		if (navigator.languages && navigator.languages.length) {
+			locale = navigator.languages[0]
+		} else if (navigator.userLanguage) {
+			locale = navigator.userLanguage
+		} else {
+			locale = navigator.language
+		}
+		return locale
+	}
 }
