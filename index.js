91a32d57c53bfc87c744c86c43069f29b56494d0
edouard.pagnier <edouard.pagnier@richemont.com>
2018-08-07 17:51:25 +0200

    auto detect locale

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-import locale from 'browser-locale'
+import getLocale from 'browser-locale'
 
-export function getLocale() {
-	return locale()
+export default class I18n {
+
+    static translations
+    static locale
+
+    static setTranslations(translations) {
+        this.translations = translations
+        this.locale = getLocale()
+    }
+
+    static t(key) {
+    	if(this.locale in this.translations) {
+    		if(key in this.translations[this.locale]) {
+    			return this.translations[this.locale][key]
+    		} else {
+    			return 'No ' + this.locale + ' translation for ' + key
+    		}
+    	} else {
+    		return 'No translation for ' + this.locale
+    	}
+    }
 }
